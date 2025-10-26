e0980fea9425f06ebe6e40ab4dc8a625fd6110b6
Aditya <adityajhunjhunwala13@gmail.com>
2025-10-27 00:09:11 +0530

    Remove commented-out MongoDB URI from database configuration

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 
-// const monogoURI =
-//   "mongodb+srv://adityajhunjhunwalacse23:5x0CgHv2EXzHvDZR@tindercluster.yxovgjs.mongodb.net/devTinder";
 const connectDB = async () => {
   await mongoose.connect(process.env.DB_CONNECTION_SECRET);
 };
