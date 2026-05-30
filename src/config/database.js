46179bdf1029fb58ea38131a83c84865db326f1a
Aditya <adityajhunjhunwala13@gmail.com>
2026-05-30 19:44:40 +0530

    Made mongoURI visible for testing

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
-  await mongoose.connect(process.env.DB_CONNECTION_SECRET);
+  await mongoose.connect(
+    "mongodb+srv://adityajhunjhunwalacse23:OMshrishyam4888@tindercluster.yxovgjs.mongodb.net/devTinder",
+  );
 };
 module.exports = connectDB;
